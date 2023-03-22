7e71aaf336f72bcb8912aa03fc41ccdc23854f8e
Ruben <rulogo94@icloud.com>
2023-03-22 18:43:07 +0100

    Add Data Types

--- 03-data-types/05-maps-and-sets/script.js
+++ 03-data-types/05-maps-and-sets/script.js
@@ -1,4 +1,6 @@
-"use strict"
+"use strict";
+
+// Maps
 
 let map = new Map();
 
@@ -13,7 +15,7 @@ console.log(map.size);
 
 // Map can use objects as keys
 
-let ruben = {name: "Ruben"};
+let ruben = { name: "Ruben" };
 
 let petsCountMap = new Map();
 
@@ -25,49 +27,78 @@ console.log(petsCountMap.get(ruben)); // 2
 
 map = new Map();
 
-map.set("1", "str1")
-	.set(1, "num1")
-	.set(true, "bool1");
+map.set("1", "str1").set(1, "num1").set(true, "bool1");
 
 // Iteration
 
 let recipeMap = new Map([
-	["cucumber", 500],
-	["tomatoes", 350],
-	["onion", 50]
+    ["cucumber", 500],
+    ["tomatoes", 350],
+    ["onion", 50],
 ]);
 
 for (let vegetable of recipeMap.keys()) {
-	console.log(vegetable); // cucumber, tomatoes, onion
+    console.log(vegetable); // cucumber, tomatoes, onion
 }
 
-for (let amount of recipeMap.values()){
-	console.log(amount); // 500, 350, 50
+for (let amount of recipeMap.values()) {
+    console.log(amount); // 500, 350, 50
 }
 
 for (let entry of recipeMap) {
-	console.log(entry); // cucumber,500 etc
+    console.log(entry); // cucumber,500 etc
 }
 
 recipeMap.forEach((value, key, map) => {
-	console.log(`${key}: ${value}`)
+    console.log(`${key}: ${value}`);
 });
 
 let obj = {
-	name: "Ruben",
-	age: 28,
-}
+    name: "Ruben",
+    age: 28,
+};
 
 let objMap = new Map(Object.entries(obj));
 
 console.log(map.get("name")); // Ruben
 
 recipeMap = new Map([
-	["cucumber", 500],
-	["tomatoes", 350],
-	["onion", 50]
+    ["cucumber", 500],
+    ["tomatoes", 350],
+    ["onion", 50],
 ]);
 
 obj = Object.fromEntries(recipeMap);
 
 console.log(obj["cucumber"]); // 500
+
+// Sets
+
+let set = new Set();
+
+ruben = { name: "Ruben" };
+let peter = { name: "Peter" };
+let aegon = { name: "Aegon" };
+
+set.add(ruben);
+set.add(peter);
+set.add(aegon);
+set.add(ruben);
+
+console.log(set.size); // 3
+
+// Iteration
+
+set = new Set(["oranges", "apples", "bananas"]);
+
+for (let value of set) {
+    console.log(value);
+}
+
+set.forEach((value, valueAgain, set) => {
+    console.log(value);
+});
+
+console.log(set.keys()); // Iterable object for values
+console.log(set.values()); // For compatibility with Map
+console.log(set.entries()); // For compatibility with Map
---
+++ 03-data-types/06-weakmaps-and-weaksets/script.js
@@ -0,0 +1,77 @@
+"use strict"
+
+// The difference between Map and WeakMap is that keys must be objects
+
+let weakMap = new weakMap();
+
+let obj = {};
+
+weakMap.set(obj, "ok");
+
+// weakMap.set("string", "Error");
+
+// Example of usage
+
+let visitCountMap = new Map();
+
+function countUser(user) {
+	let count = visitCountMap.get(user) || 0;
+	visitCountMap.set(user, count + 1);
+}
+
+let ruben = {name: "Ruben"};
+
+countUser(ruben);
+
+ruben = null; // Ruben remains in memory, it is a key in visitCountMap!!!!
+
+visitCountMap = new WeakMap();
+
+ruben = {name: "Ruben"};
+
+countUser(ruben);
+
+ruben = null; // Garbage collected!!!!
+
+// Another example: Using cache
+
+// cache.js
+let cache = new WeakMap();
+
+function process(obj) {
+	if(!cache.has(obj)) {
+		let result = "my calculations";
+
+		cache.set(obj, result);
+		return result;
+	} else {
+		return cache.get(obj);
+	}
+}
+
+// main.js
+
+obj = {name: "Ruben"};
+
+let result1 = process(obj);
+let result2 = process(obj2);
+
+obj = null; // Deleted from cache!!!!
+
+// WeakSet
+
+let visitedSet = new WeakSet();
+
+ruben = {name: "Ruben"};
+let peter = {name: "Peter"};
+let aegon = {name: "Aegon"};
+
+visitedSet.add(ruben);
+visitedSet.add(peter);
+visitedSet.add(aegon);
+visitedSet.add(ruben);
+
+ruben = null;
+
+console.log(visitedSet.has(ruben)); // false!!!!
+
